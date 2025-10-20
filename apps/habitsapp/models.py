e4faaf62e1ef5a4c58e4bb92078f5e43fd26d7c3
Savenok <dimas.savenok1990@gmail.com>
2025-10-20 20:40:15 +0300

    create project and model habit

---
+++ apps/habitsapp/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from apps.mainapp.models import BaseModel
+
+User = get_user_model()
+
+class Habit(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
+    place = models.CharField(max_length=255)
+    time = models.TimeField()
+    action = models.CharField(max_length=255)
+    is_pleasant = models.BooleanField(default=False)
+    related_habit = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='children')
+    period_days = models.PositiveSmallIntegerField(default=1)
+    reward = models.TimeField(null=True, blank=True)
+    duration_seconds = models.PositiveSmallIntegerField(default=60)
+    is_public = models.BooleanField(default=False)
+
+    def clean(self):
+        if self.reward and self.related_habit:
+            raise ValidationError('Нельзя одновременный выбор связанной привычки и указания вознаграждения')
+        if self.duration_seconds and self.duration_seconds > 120:
+            raise ValidationError('Время выполнения должно быть не больше 120 секунд.')
+        if (self.related_habit and self.related_habit.is_pleasant) or self.related_habit.id != self.id:
+            raise ValidationError('В связанные привычки могут попадать только привычки с признаком приятной привычки.')
+        if self.is_pleasant and (self.reward or self.related_habit):
+            raise ValidationError('У приятной привычки не может быть вознаграждения или связанной привычки.')
+        if not (1 <= self.period_days <= 7):
+            raise ValidationError('Нельзя выполнять привычку реже, чем 1 раз в 7 дней.')
+
---
+++ apps/mainapp/models.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        abstract = True
